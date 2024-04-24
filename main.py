3b2427543d96f56c4dc91ab1dba61d4ed964ed51
rohit151990 <rchauhan@elevondata.com>
2024-04-24 20:24:03 +0530

    testing

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+
+from fastapi import Request
+app = FastAPI()
+
+
+
+@app.get("/")
+def home():
+    return {"message": "This is second app"}
+@app.post("/items")
+async def create_item(request: Request):
+    data = await request.json()
+    print('event data is ---------------------------')
+    print(data)
+    return data
