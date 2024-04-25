89b50982c28d99e0bf6358567393097ee0b28100
rohit151990 <rchauhan@elevondata.com>
2024-04-25 15:31:57 +0530

    requirement.txt change

--- app/routers/emrouter.py
+++ app/routers/emrouter.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get("/")
 def home():
-    return {"message": "This is second app "+str(os.environ.get('MY_NAME'))}
+    return {"message": str(os.environ.get('$_MYNAME'))+"This is second app "+str(os.environ.get('MY_NAME'))}
 @router.post("/items")
 async def create_item(request: Request):
     data = await request.json()
