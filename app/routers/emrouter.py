e5dbb4c47f123b5ebc9f137efd5d59880684ccf1
rohit151990 <rchauhan@elevondata.com>
2024-04-25 13:24:42 +0530

    eventapp

--- app/routers/emrouter.py
+++ app/routers/emrouter.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 from fastapi import Request
+import os
 router = APIRouter()
 
 
 
 @router.get("/")
 def home():
-    return {"message": "This is second app"}
+    return {"message": "This is second app "+os.environ.get('MY_NAME')}
 @router.post("/items")
 async def create_item(request: Request):
     data = await request.json()
