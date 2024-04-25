cafe9fa63ef2acf653e64e7f814771a461c8e475
rohit151990 <rchauhan@elevondata.com>
2024-04-25 19:19:33 +0530

    load scrert

--- app/routers/emrouter.py
+++ app/routers/emrouter.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get("/")
 def home():
-    return {"message": str(os.environ.get('_MYNAME'))+"  - This is second app "}
+    return {"message": str(os.environ.get('_PRIVATEKEY'))+"  --------------------- This is second app -------------------------"+str(os.environ.get('_AUTHURI'))}
 @router.post("/items")
 async def create_item(request: Request):
     data = await request.json()
