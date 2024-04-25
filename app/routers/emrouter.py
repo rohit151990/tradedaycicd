8d7fb21209fc010be3c7d99ea6450d62c37756df
rohit151990 <rchauhan@elevondata.com>
2024-04-25 17:46:34 +0530

    eventapp

--- app/routers/emrouter.py
+++ app/routers/emrouter.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get("/")
 def home():
-    return {"message": str(os.environ.get('_MYNAME'))+str(os.environ.get('$_MYNAME'))+"This is second app "+str(os.environ.get('MY_NAME'))}
+    return {"message": str(os.environ.get('_MYNAME'))+"  - This is second app "}
 @router.post("/items")
 async def create_item(request: Request):
     data = await request.json()
